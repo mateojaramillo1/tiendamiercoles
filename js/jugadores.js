bdce46673b12940cd31dd0a1fca4d995af2dbd06
206 <206@academicos.cesde.edu.co>
2022-03-16 07:40:03 -0500

    se crea toda la tienda

---
+++ js/jugadores.js
@@ -0,0 +1,141 @@
+let jugadores=[
+
+    {
+        foto: '../img/neuer.webp',
+        nombre: "Manuel Neuer"
+    },
+
+    {
+        foto: '../img/pavard.webp',
+        nombre: "Benjamin Pavard"
+    },
+
+    {
+        foto: '../img/sule.webp',
+        nombre: "Niklas Süle"
+    },
+
+    {
+        foto: '../img/lucas.webp',
+        nombre: "Lucas Hernández"
+    },
+
+    {
+        foto: '../img/joshua.webp',
+        nombre: "Joshua Kimmich"
+    },
+
+    {
+        foto: '../img/sane.webp',
+        nombre: "Leroy Sané "
+    },
+
+    {
+        foto: '../img/coman.webp',
+        nombre: "Kingsley Coman"
+    },
+
+    {
+        foto: '../img/serge.webp',
+        nombre: "Serge Gnabry"
+    },
+
+    {
+        foto: '../img/leon.webp',
+        nombre: "Leon Goretzka"
+    },
+
+    {
+        foto: '../img/thomas.webp',
+        nombre: "Thomas Müller"
+    },
+
+    {
+        foto: '../img/robert.webp',
+        nombre: "Robert Lewandowsk"
+    },
+
+
+]
+
+
+let equipo = document.getElementById("equipo")
+
+jugadores.forEach(function(jugador){
+
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card")
+    tarjeta.classList.add("h-100")
+
+    let foto=document.createElement("img")
+    foto.classList.add("card-img-top")
+    foto.src=jugador.foto
+
+    let titulo = document.createElement("h4")
+    titulo.classList.add("text-center")
+    titulo.textContent=jugador.nombre
+
+    tarjeta.appendChild(foto)
+    tarjeta.appendChild(titulo)
+    columna.appendChild(tarjeta)
+    equipo.appendChild(columna)
+
+
+})
+
+
+let uniformes=[
+
+    {
+        foto: '../img/titular.jpg',
+        titulo:"Equipacion titular"
+    },
+
+    {
+        foto: '../img/visitante.jpg',
+        titulo:"Equipacion visitante"
+    },
+
+    {
+        foto: '../img/portero.jpg',
+        titulo:"Equipacion portero"
+    },
+
+
+]
+
+
+let equipaciones = document.getElementById("equipaciones")
+
+uniformes.forEach(function(uniforme){
+
+
+    console.log(uniformes)
+
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card")
+    tarjeta.classList.add("h-100")
+
+    let foto=document.createElement("img")
+    foto.classList.add("card-img-top")
+    foto.src=uniforme.foto
+
+    let titulo = document.createElement("h4")
+    titulo.classList.add("text-center")
+    titulo.textContent=uniforme.titulo
+
+    tarjeta.appendChild(foto)
+    tarjeta.appendChild(titulo)
+    columna.appendChild(tarjeta)
+    equipaciones.appendChild(columna)
+
+
+})
+
+
--- js/llenadoTienda.js
+++ js/llenadoTienda.js
@@ -5,71 +5,82 @@ let productos=[
     {
         foto: '../img/camiseta.webp',
         nombre:"Camiseta Bayern Múnich",
-        precio:350000, 
-        descripcion:"Camiseta del FC BAYERN local Autentico"
+        precio:'$ 350.000', 
+        descripcion:"Camiseta del FC BAYERN local Autentico",
+        boton: 'boton1'
     },
 
     {
         foto: '../img/gorra.webp',
         nombre:"Gorra Bayern Múnich ",
-        precio:70000,
-        descripcion:"Logotipo de la Gorra 5 Estrellas"
+        precio:'$ 70.000',
+        descripcion:"Logotipo de la Gorra 5 Estrellas",
+        boton: 'boton2'
     },
 
     {
         foto: '../img/camiseta_manga_larga.webp',
         nombre:"camisa manga larga Bayern Múnich",
-        precio:300000,
-        descripcion:"Camiseta de manga larga de local FC BAYERN MÜNCHEN 21/22"
+        precio:'$ 300.000',
+        descripcion:"Camiseta de manga larga de local FC BAYERN MÜNCHEN 21/22",
+        boton: 'boton3'
     },
 
     {
         foto: '../img/chaqueta.webp',
         nombre:"chaqueta Bayern Múnich",
-        precio:380000,
-        descripcion:"Chaqueta de entrenamiento del FCB SPORTS"
+        precio:'$ 380.000',
+        descripcion:"Chaqueta de entrenamiento del FCB SPORTS",
+        boton: 'boton4'
     },
 
     {
         foto: '../img/pantaloneta.webp',
         nombre:"pantaloneta Bayern Múnich",
-        precio:180000,
-        descripcion:"Pantaloneta ADIDAS LIFESTYLE"
+        precio:'$ 180.000',
+        descripcion:"Pantaloneta ADIDAS LIFESTYLE",
+        boton: 'boton5'
     },
 
     {
         foto: '../img/bolso.webp',
         nombre:"Bolso Bayern Múnich",
-        precio:250000, 
-        descripcion:"Mochila 5 estrellas logotipo"
+        precio:'$ 250.000', 
+        descripcion:"Mochila 5 estrellas logotipo",
+        boton: 'boton6'
     },
 
     {
         foto: '../img/camiseta_polo.webp',
         nombre: "Camiseta tipo polo Bayern Múnich",
-        precio:210000,
-        descripcion:"Polo funcional FCB SPORTS"
+        precio:'$ 210.000',
+        descripcion:"Polo funcional FCB SPORTS",
+        boton: 'boton7'
+
     },
 
     {
         foto: '../img/termo.webp',
         nombre: "Termo Bayern Múnich",
-        precio:60000,
-        descripcion:"Botella multifuncional 7 en 1"
+        precio:'$ 60.000',
+        descripcion:"Botella multifuncional 7 en 1",
+        boton: 'boton8'
     },
 
     {
         foto: '../img/reloj.webp',
         nombre: "Reloj Bayern Múnich",
-        precio:140000,
-        descripcion:"Reloj digital"
+        precio:'$ 140.000',
+        descripcion:"Reloj digital",
+        boton: 'boton9'
     },
 
     {
         foto: '../img/camiseta_dama.webp',
         nombre: "Camiseta dama Bayern Múnich",
-        precio:210000,
-        descripcion:"Camiseta del FC Bayern para damas Champions League 21/22"
+        precio:'$ 210.000',
+        descripcion:"Camiseta del FC Bayern para damas Champions League 21/22",
+        boton: 'boton10'
     },
     
     
@@ -103,6 +114,7 @@ productos.forEach(function(producto){
     console.log(producto.foto)
     console.log(producto.nombre)
     console.log(producto.precio)
+    console.log(producto.boton)
     
 
     //pintando etiquetas
@@ -131,10 +143,41 @@ productos.forEach(function(producto){
     titulo.classList.add("text-center")
     titulo.textContent=producto.nombre
 
+    // h5 con la clase de text-center
+
+    let precio = document.createElement("h5")
+    precio.classList.add("text-center")
+    precio.textContent=producto.precio
+
+    //h6 con la clase text-center
+
+    let descripcion = document.createElement("h6")
+    descripcion.classList.add("text-center")
+    descripcion.textContent=producto.descripcion
+
+
+    //boton
+
+    let boton = document.createElement("button")
+    boton.classList.add("btn")
+    boton.classList.add("boton")
+    boton.classList.add("btn-outline-success")
+    boton.textContent= "Ver producto"
+    
+    
+
+    
+
+
+    
+
     //3 padres e hijos
 
     tarjeta.appendChild(foto)
     tarjeta.appendChild(titulo)
+    tarjeta.appendChild(precio)
+    tarjeta.appendChild(descripcion)
+    tarjeta.appendChild(boton)
     columna.appendChild(tarjeta)
     fila.appendChild(columna)
 
